4d46cb2a5009a2f1c408df35593e70f39ee19e70
vipex22 <66770038+vipex22@users.noreply.github.com>
2021-02-03 17:27:43 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,18 +7,19 @@
   try{
    $myPDO = new PDO("pgsql:host=ec2-54-156-121-142.compute-1.amazonaws.com;dbname=dqd7oa5e19ldm","bidifaumdbqryy","d7f81c395bc38e5c760c2f7163149463308f70cb36fd9423c9b2f41d6704a535");
     echo "Connected";
-   $query = "SELECT * FROM test_lab6 LIMIT 2";
-   $rs = pg_query($myPDO, $query)
+  }catch(PDOException $e)
+  {
+   echo $e->getMessage();
+  }
+  $query = "SELECT * FROM test_lab6 LIMIT 5"; 
+
+$rs = pg_query($myPDO, $query) or die("Cannot execute query: $query\n");
+
 while ($row = pg_fetch_row($rs)) {
   echo "$row[0] $row[1] $row[2]\n";
 }
 
 pg_close($con); 
-
-  }catch(PDOException $e)
-  {
-   echo $e->getMessage();
-  }
 
 
 
