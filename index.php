22679a34ef21c415cf4d8bab9b8ab55f51eac0ee
vipex22 <66770038+vipex22@users.noreply.github.com>
2021-02-03 17:00:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
  <body>
  <?php echo '<p>Hello Quang!</p>'; 
   try{
-   $myPDO = new PDO("pgsql:host=localhost;dbname=Lab6","dqd7oa5e19ldm","d7f81c395bc38e5c760c2f7163149463308f70cb36fd9423c9b2f41d6704a535")
-    echo "Connected"
+   $myPDO = new PDO("pgsql:host=localhost;dbname=Lab6","dqd7oa5e19ldm","d7f81c395bc38e5c760c2f7163149463308f70cb36fd9423c9b2f41d6704a535");
+    echo "Connected";
   }catch(PDOException $e)
   {
    echo $e->getMessage();
