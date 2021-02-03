4ed1f34dc4f9f99b1f103afec0b5936b6a127657
vipex22 <66770038+vipex22@users.noreply.github.com>
2021-02-03 17:24:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,25 @@
    $myPDO = new PDO("pgsql:host=ec2-54-156-121-142.compute-1.amazonaws.com;dbname=dqd7oa5e19ldm","bidifaumdbqryy","d7f81c395bc38e5c760c2f7163149463308f70cb36fd9423c9b2f41d6704a535");
     echo "Connected";
    $query = "SELECT * FROM test_lab6 LIMIT 2";
-   $rs = pg_query($myPDO, $query);
+   $rs = pg_query($myPDO, $query)
+   echo "<table border='1'>
+<tr>
+<th>ProductID</th>
+<th>ProductName</th>
+<th>ProductPrice</th>
+</tr>";
+
+while($row = pg_fetch_array($rs))
+{
+echo "<tr>";
+echo "<td>" . $row['product_id'] . "</td>";
+echo "<td>" . $row['product_name'] . "</td>";
+echo "<td>" . $row['product_price'] . "</td>";
+echo "</tr>";
+}
+echo "</table>";
+
+mysqli_close($con);
   }catch(PDOException $e)
   {
    echo $e->getMessage();
