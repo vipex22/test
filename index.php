34a0d0698df2355a893923adf5f1475927e64e24
vipex22 <66770038+vipex22@users.noreply.github.com>
2021-02-27 16:57:16 +0700

    Update index.php

--- index.php
+++ index.php
@@ -14,6 +14,7 @@
         <li> <a href="">Login </a></li>
         <li> <a href="data.php">View database</a> </li>        
     </ul>
+  </head>
     <body>
       <style>
         body {
@@ -23,3 +24,4 @@
         } 
       </style>
     </body>
+</html>
