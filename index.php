19b085030981a12a91b1df407902735d6890ecd9
vipex22 <66770038+vipex22@users.noreply.github.com>
2021-02-27 16:26:23 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+  <head>
+    <link rel="stylesheet" href ="styles.css">
+    <title> ATN Shop </title>
+    <marquee bgcolor="yellow" 
+             scrollamount="12" 
+             direction="left" 
+             onmouseover="this.stop();" 
+             onmouseout="this.start();">
+        [2/27/2021]: Remember to check the newest release!
+    </marquee>
