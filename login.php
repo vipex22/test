85e0b0ea1b4048d184cec4fc1fd509bad8d8a924
vipex22 <66770038+vipex22@users.noreply.github.com>
2021-02-27 16:58:30 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,1 +1,25 @@
-
+  <head>
+    <link rel="stylesheet" href ="styles.css">
+    <title> ATN Shop </title>
+    <marquee bgcolor="gray" 
+             scrollamount="12" 
+             direction="left" 
+             onmouseover="this.stop();" 
+             onmouseout="this.start();">
+        [2/27/2021]: Remember to check the database everyday!
+    </marquee>
+    <ul> 
+        <li> <a href="index.php">Main Page</a></li>
+        <li> <a href="">Login </a></li>
+        <li> <a href="data.php">View database</a> </li>        
+    </ul>
+  </head>
+    <body>
+      <style>
+        body {
+          background-image: url('background.jpg');
+          background-attachment: fixed;
+          background-size: 100%100%;
+        } 
+      </style>
+    </body>
