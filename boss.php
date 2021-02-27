139c7ce3aaa83e7dbff9fb0d7c32c00a6cd4bb24
vipex22 <66770038+vipex22@users.noreply.github.com>
2021-02-27 22:43:54 +0700

    Update boss.php

--- boss.php
+++ boss.php
@@ -62,6 +62,46 @@
 			}
 	    
 		?> 
+		<table border="2">
+		    <tr>
+			    <th>Product ID</th>
+			    <th>Product Name</th>
+			    <th>Product Price</th>
+			    <th>Quantity</th>
+		    </tr>
+		<?php 
+			echo '<p>ATN Shop 2</p>'; 
+			$host_heroku = "ec2-54-156-121-142.compute-1.amazonaws.com";
+			$db_heroku = "dqd7oa5e19ldm";
+			$user_heroku = "bidifaumdbqryy";
+			$pw_heroku = "d7f81c395bc38e5c760c2f7163149463308f70cb36fd9423c9b2f41d6704a535";
+			$conn_string = "host=$host_heroku port=5432 dbname=$db_heroku user=$user_heroku password=$pw_heroku";
+			$pg_heroku = pg_connect($conn_string);
+			
+			if (!$pg_heroku)
+			{
+				die('Error: Could not connect: ' . pg_last_error());
+			}
+		    	$query = 'select * from atnshop2';
+			$data = pg_query($pg_heroku, $query);
+	    		$total = pg_num_rows($data);
+			if($total!=0)
+			{
+				while ($result=pg_fetch_assoc($data))
+				{
+					echo "
+					<tr>
+					<td>".$result['productid']."</td>
+					<td>".$result['productname']."</td>
+					<td>".$result['productprice']."</td>
+					<td>".$result['quantityonhand']."</td>
+					
+					</tr>
+					";
+				}
+			}
+	    
+		?> 
 		 
 	</body>
 </html>
